c615e72c89622845a73274f37436a7a50593b093
David Brady <dgrbrady@gmail.com>
2020-04-02 17:15:39 -0400

    Adding @angular/elements

---
+++ projects/paper-icons/src/lib/custom-element.module.ts
@@ -0,0 +1,15 @@
+import { Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { PREFIX } from './prefix';
+
+export abstract class ElementModule {
+  constructor(injector: Injector, component: InstanceType<any>, name: string) {
+    const ngElement = createCustomElement<typeof component>(component, {
+      injector,
+    });
+
+    customElements.define(`${PREFIX}-${name}`, ngElement);
+  }
+
+  ngDoBootstrap() {}
+}
