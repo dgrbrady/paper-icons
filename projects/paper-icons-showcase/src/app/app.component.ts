805948e51a1303f5455044371facdbe6be89d2b3
David Brady <dgrbrady@gmail.com>
2020-04-02 08:08:31 -0400

    Adding copy to clipboard functionality

--- projects/paper-icons-showcase/src/app/app.component.ts
+++ projects/paper-icons-showcase/src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import anime from 'animejs';
-import { PaperBadges, PaperItems } from 'paper-icons';
+import { PaperBadges, PaperIconComponent, PaperItems } from 'paper-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,18 +18,19 @@ export class AppComponent implements OnInit {
   itemIcons = Object.values(PaperItems).map(icon => icon.name);
   badgeIcons = Object.values(PaperBadges).map(icon => icon.name);
 
-  constructor(private elRef: ElementRef) {}
+  @ViewChild('dialog', { static: false }) dialog: ElementRef<HTMLDialogElement>;
+
+  constructor(
+    private elRef: ElementRef,
+    @Inject(DOCUMENT) private document: Document,
+  ) {}
 
   ngOnInit() {
     const textElement: HTMLElement = this.elRef.nativeElement.querySelector(
-      '.dialogue-text',
+      '.dialog-text',
     );
     // wrap each letter in a span so we can animate it
-    textElement.innerHTML = textElement.textContent.replace(
-      /\S/g,
-      '<span class="letter">$&</span>',
-    );
-    const letters = textElement.querySelectorAll('.letter');
+    const letters = this.getLettersForDialog(textElement);
     const timelineDuration = letters.length * 1.25;
     const timeline = anime.timeline({ duration: timelineDuration });
     letters.forEach(letter =>
@@ -31,6 +39,15 @@ export class AppComponent implements OnInit {
         opacity: [0, 1],
       }),
     );
+  }
+
+  private getLettersForDialog(textElement: Element) {
+    textElement.innerHTML = textElement.textContent.replace(
+      /\S/g,
+      '<span class="letter">$&</span>',
+    );
+    const letters = textElement.querySelectorAll('.letter');
+    return letters;
   }
 
   wobble(event: MouseEvent) {
@@ -52,5 +69,25 @@ export class AppComponent implements OnInit {
       easing: 'easeInOutSine',
       duration: 250,
     });
+  }
+
+  copyToClipBoard(icon: PaperIconComponent) {
+    const exampleCode = this.document.querySelector(`#${icon.name}_example`);
+    this.document.defaultView.navigator.clipboard.writeText(
+      exampleCode.textContent,
+    );
+    const textElement = this.dialog.nativeElement.querySelector('.dialog-text');
+    const letters = this.getLettersForDialog(textElement);
+    const timelineDuration = letters.length * 1.25;
+    const timeline = anime.timeline({ duration: timelineDuration });
+    letters.forEach(letter =>
+      timeline.add({
+        targets: letter,
+        opacity: [0, 1],
+        begin: () => (this.dialog.nativeElement.open = true),
+        complete: () =>
+          setTimeout(() => (this.dialog.nativeElement.open = false), 1200),
+      }),
+    );
   }
 }
