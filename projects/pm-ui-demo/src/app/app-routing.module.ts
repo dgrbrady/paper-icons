53824d81348631cf8a72b0a33d3e28514190a079
David Brady <dgrbrady@gmail.com>
2020-05-27 17:13:55 -0400

    added intro page to demo

--- projects/pm-ui-demo/src/app/app-routing.module.ts
+++ projects/pm-ui-demo/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ButtonPageComponent } from 'projects/pm-ui-demo/src/app/button-page/button-page.component';
+import { IntroPageComponent } from 'projects/pm-ui-demo/src/app/intro-page/intro-page.component';
 
 export const routes: Routes = [
+  { path: '', component: IntroPageComponent },
   { path: 'button', component: ButtonPageComponent },
 ];
 
--- projects/pm-ui-demo/src/app/app.module.ts
+++ projects/pm-ui-demo/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from 'projects/pm-ui-demo/src/app/app-routing.module
 import { PmUiModule } from '../../../pm-ui/src/public-api';
 import { AppComponent } from './app.component';
 import { ButtonPageComponent } from './button-page/button-page.component';
+import { IntroPageComponent } from './intro-page/intro-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonPageComponent],
+  declarations: [AppComponent, ButtonPageComponent, IntroPageComponent],
   imports: [BrowserModule, PmUiModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
