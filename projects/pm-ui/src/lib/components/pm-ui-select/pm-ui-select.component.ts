77a8fd82d9790afb99959f79affaf4148d017834
David Brady <dgrbrady@gmail.com>
2020-06-08 22:09:03 -0400

    Basic select functionality

--- projects/pm-ui/src/lib/components/pm-ui-select/pm-ui-select.component.ts
+++ projects/pm-ui/src/lib/components/pm-ui-select/pm-ui-select.component.ts
@@ -1,8 +1,14 @@
+import { DOCUMENT } from '@angular/common';
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
+  ElementRef,
+  Inject,
   Input,
   OnInit,
+  Renderer2,
+  ViewChild,
 } from '@angular/core';
 
 @Component({
@@ -15,13 +21,42 @@ export class PmUiSelectComponent implements OnInit {
   @Input() label: string;
   @Input() value: any;
   @Input() options: any[];
+  @ViewChild('optionsContainer', { static: true }) optionsContainer: ElementRef<
+    HTMLElement
+  >;
+  @ViewChild('wrapper', { static: true }) wrapper: ElementRef<HTMLElement>;
   showOptions = false;
-  constructor() {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer2: Renderer2,
+    private cdRef: ChangeDetectorRef,
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.document.defaultView.addEventListener('click', event => {
+      const target: HTMLElement = event.target as HTMLElement;
+      const input: HTMLInputElement = this.wrapper.nativeElement.querySelector(
+        'input',
+      );
+      if (target.id !== input.id) {
+        this.showOptions = false;
+        this.cdRef.markForCheck();
+      }
+    });
+  }
+
   toggleOptions() {
     this.showOptions = !this.showOptions;
+    const wrapperWidth = this.document.defaultView.getComputedStyle(
+      this.wrapper.nativeElement,
+    ).width;
+    this.renderer2.setStyle(
+      this.optionsContainer.nativeElement,
+      'width',
+      wrapperWidth,
+    );
   }
+
   setValue(newValue: any) {
     this.value = newValue;
   }
